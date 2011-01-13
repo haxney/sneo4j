90fd64d6123296d7939958f958d31e9aca5981e1
Dan Hackney <dan@haxney.org>
2011-01-12 21:42:50 -0500

    Rename Serialize{Test => Spec}
    Since it uses the Specs library.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- src/test/scala/org/sneo4j/test/SerializeSpec.scala
+++ src/test/scala/org/sneo4j/test/SerializeSpec.scala
@@ -1,12 +1,10 @@
 package org.sneo4j.test
 
 import org.specs.Specification
-import org.codehaus.jackson.map.ObjectMapper
-import org.codehaus.jackson.JsonNode
+import sjson.json.Serializer.{SJSON => sj}
 import org.sneo4j.types._
 
 class SerializeTest extends Specification {
-  val mapper = new ObjectMapper()
   val rootString = """{"node":"node-path","extensions_info":"ext-info","reference_node":"ref-node","relationship_index":"relation-index","node_index":"node-index"}"""
   val root = GetRoot("relation-index", "node-path", "ext-info", "node-index", "ref-node")
 
@@ -20,5 +18,11 @@ class SerializeTest extends Specification {
     "deserialize from JSON" in {
       mapper.readValue(rootString, classOf[GetRoot]) mustEqual root
     }
+  }
+
+
+  "A GetNode object" should {
+    "serialize to JSON"
+    "deserialze from JSON
   }
 }
