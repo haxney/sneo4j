3794e2c8a1c41ec52a0224489ea0370a617ed0db
Dan Hackney <dan@haxney.org>
2011-01-12 21:43:35 -0500

    Swap SJson for Sjersey.
    It is a nicer JSON library.
    
    Signed-off-by: Dan Hackney <dan@haxney.org>

--- project/build/Sneo4j.scala
+++ project/build/Sneo4j.scala
@@ -4,7 +4,8 @@ class Sneo4j(info: ProjectInfo) extends DefaultProject(info)
 {
   val codeHaus = "Codehaus" at "http://repository.codehaus.org"
   val scalaToolsReleases = "Scala-Tools Maven2 Releases Repository" at "http://scala-tools.org/repo-releases"
-  val jackson = "org.codehaus.jackson" % "jackson-jaxrs" % "1.7.0"
-  val jerseyClient = "com.sun.jersey" % "jersey-client" % "1.4"
-val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7"
+  val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7"
+
+  val sjson = project("sjson")
+
 }
